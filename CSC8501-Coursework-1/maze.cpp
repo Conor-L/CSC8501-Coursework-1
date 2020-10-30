88afa41c7540a34dc463250153e4fc4b97a9c96e
Conor Lambert <lambert.conor123@gmail.com>
2020-10-30 18:34:08 +0000

    Maze Generation Update
    -Converted Char array to Cell array and added different members to the Cell Struct
    -Cell Struct now contains all 4 neighbours of a cell (nullptr default value if there isn't a possible neighbour available)
    -Cell Struct now contains a Char value member so that the original value that was first stored in the Char* is now in the cell and can be used as normal
    -Conversion was necessary for part 1 and part 2 of this coursework as it is needed for both the Maze generation and the A* pathfinding algorithm.

--- CSC8501-Coursework-1/maze.cpp
+++ CSC8501-Coursework-1/maze.cpp
@@ -6,9 +6,9 @@ using namespace std;
 Maze::Maze() {
 	srand(time(NULL));
 
-	maze = new char*[12];
+	maze = new Cell*[12];
 	for (int i = 0; i < 12; i++) {
-		maze[i] = new char[35];
+		maze[i] = new Cell[35];
 	}
 
 	maze_x_size = 11;
@@ -17,6 +17,8 @@ Maze::Maze() {
 
 	place_exit(num_exits);
 	place_start(maze_x_size / 2, maze_y_size / 2);
+
+	set_neighbours();
 
 }
 
@@ -27,15 +29,17 @@ Maze::Maze(int dim_x, int dim_y, int num_exits) {
 	maze_y_size = dim_y - 1;
 	this->num_exits = num_exits;
 
-	maze = new char* [dim_x];
+	maze = new Cell*[dim_x];
 	for (int i = 0; i < dim_x; i++) {
-		maze[i] = new char[dim_y];
+		maze[i] = new Cell[dim_y];
 	}
 
 	fill_maze();
 
 	place_exit(num_exits);
 	place_start(maze_x_size / 2, maze_y_size / 2);
+
+	set_neighbours();
 
 }
 	
@@ -50,7 +54,35 @@ Maze::~Maze() {
 void Maze::fill_maze() {
 	for (int i = 0; i < (maze_x_size + 1); i++) {
 		for (int j = 0; j < (maze_y_size + 1); j++) {
-			maze[i][j] = 'X';
+			maze[i][j].value = 'X';
+			maze[i][j].x = i;
+			maze[i][j].y = j;
+		}
+	}
+}
+
+void Maze::set_neighbours() {
+	// Set the neighours of each Cell
+	// leftmost cells don't have a left neighbour
+	// rightmost cells don't have a right neighbour
+
+	for (int i = 0; i < (maze_x_size + 1); i++) {
+		for (int j = 0; j < (maze_y_size + 1); j++) {
+			if (j == 0 || j != maze_y_size) {
+				maze[i][j].right_neighbour = &maze[i][j + 1];
+			}
+
+			if (j != 0) {
+				maze[i][j].left_neighbour = &maze[i][j - 1];
+			}
+
+			if (i == 0 || i != maze_x_size) {
+				maze[i][j].down_neighbour = &maze[i + 1][j];
+			}
+
+			if (i != 0) {
+				maze[i][j].up_neighbour = &maze[i - 1][j];
+			}
 		}
 	}
 }
@@ -74,7 +106,7 @@ void Maze::move_cell(Cell current_cell) {
 void Maze::print_maze() {
 	for (int i = 0; i < (maze_x_size + 1); i++) {
 		for (int j = 0; j < (maze_y_size + 1); j++) {
-			cout << maze[i][j];
+			cout << maze[i][j].value;
 		}
 		cout << endl;
 	}
@@ -111,34 +143,34 @@ void Maze::place_exit(int num_exits) {
 
 		// Make sure the exits are not in the corners otherwise there is no valid path
 		if ((x_C == 0 && y_C == 0) || (x_C == 0 && y_C == maze_y_size)) {
-			if (maze[x_C + 1][y_C] == 'E') {
+			if (maze[x_C + 1][y_C].value == 'E') {
 				i -= 1;
 			}
 
 			else {
-				maze[x_C + 1][y_C] = 'E';
+				maze[x_C + 1][y_C].value = 'E';
 				exit_vector.emplace_back(create_exit_cell(x_C + 1, y_C));
 			}
 		}		
 
 		else if ((x_C == maze_x_size && y_C == 0) || (x_C == maze_x_size && y_C == maze_y_size)) {
-			if (maze[x_C - 1][y_C] == 'E') {
+			if (maze[x_C - 1][y_C].value == 'E') {
 				i -= 1;
 			}
 
 			else {
-				maze[x_C - 1][y_C] = 'E';
+				maze[x_C - 1][y_C].value = 'E';
 				exit_vector.emplace_back(create_exit_cell(x_C - 1, y_C));
 			}
 		}
 
 		else {
-			if (maze[x_C][y_C] == 'E') {
+			if (maze[x_C][y_C].value == 'E') {
 				i -= 1;
 			}
 
 			else {
-				maze[x_C][y_C] = 'E';
+				maze[x_C][y_C].value = 'E';
 				exit_vector.emplace_back(create_exit_cell(x_C, y_C));
 			}
 		}
@@ -147,7 +179,7 @@ void Maze::place_exit(int num_exits) {
 }
 
 void Maze::place_start(int startx, int starty) {
-	maze[startx][starty] = 'S';
+	maze[startx][starty].value = 'S';
 	starting_cell.x = startx;
 	starting_cell.y = starty;
 }
--- CSC8501-Coursework-1/maze.h
+++ CSC8501-Coursework-1/maze.h
@@ -1,7 +1,17 @@
 #pragma once
 #include <vector>
 
-struct Cell { int x; int y; bool visited = false; };
+struct Cell { 
+	char value = ' '; 
+	int x = 0; 
+	int y = 0; 
+	bool visited = false; 
+	bool isWall = false;
+	Cell* up_neighbour = nullptr;
+	Cell* down_neighbour = nullptr;
+	Cell* left_neighbour = nullptr;
+	Cell* right_neighbour = nullptr;
+};
 
 class Maze {	
 	public:
@@ -10,6 +20,7 @@ class Maze {
 		~Maze();
 
 		void fill_maze();
+		void set_neighbours();
 		void generate_maze();
 		void move_cell(Cell current_cell);
 		void print_maze();
@@ -20,7 +31,7 @@ class Maze {
 		int generate_random_number(int upper_limit, int lower_limit);
 
 	private:
-		char** maze;
+		Cell** maze;
 		int maze_x_size = 0;
 		int maze_y_size = 0;
 		int num_exits = 1;
